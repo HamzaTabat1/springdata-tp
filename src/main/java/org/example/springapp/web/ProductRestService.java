cd85ae72b5ad12e9cefc4c12ddb1481c145a2425
LAPTOP-GQA9U98Q\hamza <Hamza.Tabat@emsi-edu.ma>
2025-04-11 19:03:38 +0100

    final commit

--- src/main/java/org/example/springapp/web/ProductRestService.java
+++ src/main/java/org/example/springapp/web/ProductRestService.java
@@ -1,4 +1,24 @@
 package org.example.springapp.web;
 
+import org.example.springapp.entities.Product;
+import org.example.springapp.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class ProductRestService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/products")
+    public List<Product> products() {
+        return productRepository.findAll(); //
+    }
+
+    @GetMapping("/products/{id}")
+    public Product findProduct(@PathVariable Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
 }
